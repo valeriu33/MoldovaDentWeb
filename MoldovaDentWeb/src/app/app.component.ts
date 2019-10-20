31105e07f59452a531dba825dc3249015e4f7167
Seremet Valeriu <valeriu3333@gmail.com>
2019-10-20 18:34:46 +0300

    Refactor navBar animation

--- MoldovaDentWeb/src/app/app.component.ts
+++ MoldovaDentWeb/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { HostListener } from '@angular/core';
+import { ExpandMenu, MinifyMenu } from './actions/ui.actions';
+import { Store } from '@ngxs/store';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,5 +12,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'MoldovaDentWeb';
 
-  constructor() {}
+  constructor(private store: Store) {}
+
+  @HostListener('window:scroll', [])
+  onScroll() {
+    if (window.pageYOffset === 0) {
+      this.store.dispatch(new ExpandMenu());
+    } else {
+      this.store.dispatch(new MinifyMenu());
+    }
+  }
+
 }
--- MoldovaDentWeb/src/app/components/content/content.component.ts
+++ MoldovaDentWeb/src/app/components/content/content.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HostListener } from '@angular/core';
 import { Store } from '@ngxs/store';
-import { ExpandMenu, MinifyMenu } from '../../actions/ui.actions';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
@@ -16,30 +14,18 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
       state('up', style({
         paddingTop: '110px'
       })),
-      transition('up => down', [animate('500ms')]),
-      transition('down => up', [animate('500ms')])
+      transition('up <=> down', [animate('500ms')])
     ])
   ]
 })
 export class ContentComponent implements OnInit {
 
-  constructor(private store: Store) {
-    this.store.select(menuState => menuState.app.ui.isMenuExpanded)
-    .subscribe(isExpanded => isExpanded ? this.currentPosition = 'down' : this.currentPosition = 'up');
-  }
-
   currentPosition = 'down';
 
-
-  @HostListener('window:scroll', [])
-  onScroll() {
-    if (window.pageYOffset === 0) {
-      this.store.dispatch(new ExpandMenu());
-    } else {
-      this.store.dispatch(new MinifyMenu());
-    }
+  constructor(private store: Store) {
+    this.store.select(menuState => menuState.app.ui.isMenuExpanded)
+      .subscribe(isExpanded => isExpanded ? this.currentPosition = 'down' : this.currentPosition = 'up');
   }
-
   ngOnInit() {
   }
 
